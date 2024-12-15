699c7f5ac450f794b1875df75461f0a34fcef02f
Rasyaa21 <naufalrasya21907@gmail.com>
2024-12-15 23:29:29 +0700

    tipis tipis

--- resources/views/pages/frontend/store/detail.blade.php
+++ resources/views/pages/frontend/store/detail.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.store')
-
 @section('title', 'Detail Produk')
-
 @section('content')
 <section class="py-5">
     <div class="container px-4 my-5 px-lg-5">
@@ -10,17 +8,16 @@
                 <img class="mb-5 rounded card-img-top mb-md-0" src="{{ asset('storage/' . $product->thumbnail) }}" alt="{{ $product->name }}" style="max-width: 600px; max-height: 100%; object-fit: cover;"/>
             </div>
             <div class="col-md-6">
-                <div class="mb-1 small">{{ $product->slug }}</div>
-                <h1 class="mt-4 mb-3" style="font-size: 3rem; font-weight: bold;">{{ $product->name }}</h1>
-                <div class="mb-5 fs-5">
+                <div class="mb-1 text-center small text-md-start">{{ $product->slug }}</div>
+                <h1 class="mt-4 mb-3 text-center text-md-start" style="font-size: 3rem; font-weight: bold;">{{ $product->name }}</h1>
+                <div class="mb-5 text-center text-md-start fs-5">
                     <h3 class="mb-3 text-primary">Rp. {{ number_format($product->price, 0, ',', '.') }}</h3>
                 </div>
-                <p class="lead">{{ $product->description }}</p>
-                <div class="d-flex">
-                    <input class="text-center form-control me-3" id="inputQuantity" type="num" value="1" style="max-width: 3rem" />
-                    <button class="flex-shrink-0 btn btn-outline-dark" type="button">
-                        <i class="bi-cart-fill me-1"></i>
-                        Tambah ke keranjang
+                <p class="text-center lead text-md-start">{{ $product->description }}</p>
+                <div class="d-flex justify-content-center justify-content-md-start">
+                    <input class="text-center form-control me-3" id="quantity" type="num" value="1" style="max-width: 3rem" />
+                    <button class="py-2 btn btn-warning w-100" type="button">
+                        <i class="bi-cart-fill me-1"></i> Tambah ke keranjang
                     </button>
                 </div>
             </div>
