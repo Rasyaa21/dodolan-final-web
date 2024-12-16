670962b84767fa9f93918ffbdeb6542d81eebb72
Farrel44 <farrellasso69@gmail.com>
2024-12-16 23:37:57 +0700

    add midtrans

--- app/Http/Controllers/Frontend/CheckoutController.php
+++ app/Http/Controllers/Frontend/CheckoutController.php
@@ -20,6 +20,15 @@ class CheckoutController extends Controller
     {
         // Dummy response for checkout success
         return redirect()->route('pages.frontend.checkout.success')->with('success', 'Checkout successful (Dummy Data)!');
+
+        // Set your Merchant Server Key
+        \Midtrans\Config::$serverKey = config('midtrans.serverKey');
+        // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
+        \Midtrans\Config::$isProduction = false;
+        // Set sanitization on (default)
+        \Midtrans\Config::$isSanitized = true;
+        // Set 3DS transaction for credit card to true
+        \Midtrans\Config::$is3ds = true;
     }
 
     public function success()
